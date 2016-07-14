bca3ad35f5b3e69f91d908870f5c4a6c540d800f
Peter Behroozi <peter@Peters-MacBook-Pro.local>
2016-07-13 19:28:51 -0700

    Fix for issue #2.

--- check_syscalls.h
+++ check_syscalls.h
@@ -32,6 +32,7 @@ void check_mtrim(void);
 #define stringify(x) #x
 #define to_string(x) stringify(x)
 #define check_realloc_s(x,y,z) { (x) = check_realloc((x),((int64_t)(y))*((int64_t)(z)), "Reallocating " #x " at " __FILE__ ":" to_string(__LINE__)); }
+#define check_malloc_s(x,y,z) check_realloc(NULL,((int64_t)(y))*((int64_t)(z)), "Allocating " #x " at " __FILE__ ":" to_string(__LINE__));
 
 #define check_realloc_var(x,size,cur,new) { if (cur < new) { cur = new; check_realloc_s(x,size,new); } }
 #define check_realloc_every(x,size,cur,num) { if (!((cur)%(num))) { check_realloc_s(x,size,(cur)+(num)); } }
